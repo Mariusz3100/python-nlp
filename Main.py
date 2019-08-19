08626daa0f3072615e3469c230a5afdebc8c7c2c
M <armaniel667@gmail.com>
2019-08-19 14:15:11 +0200

    Small changes and command line starting script

--- Main.py
+++ Main.py
@@ -8,6 +8,6 @@ from TokenizeServer import TokenizeEndpoint
 
 api = falcon.API()
 token_endpoint = TokenizeEndpoint()
-ner_endpoint=NerEndpoint()
+ner_endpoint = NerEndpoint()
 api.add_route('/tokenizer', token_endpoint)
 api.add_route('/ner', ner_endpoint)
