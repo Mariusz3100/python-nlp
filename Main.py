1fb0e4a10afdac22520e371bf73cd8290df8b3fd
M <armaniel667@gmail.com>
2019-08-21 17:04:34 +0200

    test servers added

--- Main.py
+++ Main.py
@@ -4,10 +4,15 @@ import json
 import spacy
 
 from NerServer import NerEndpoint
+from TestServer import TestEndpoint, TestCompoundEndpoint
 from TokenizeServer import TokenizeEndpoint
 
 api = falcon.API()
 token_endpoint = TokenizeEndpoint()
 ner_endpoint = NerEndpoint()
+test_endpoint = TestEndpoint()
+
+testCompound = TestCompoundEndpoint()
 api.add_route('/tokenizer', token_endpoint)
 api.add_route('/ner', ner_endpoint)
+api.add_route('/testCompound', testCompound)
---
+++ TestServer.py
@@ -0,0 +1,27 @@
+from typing import re
+
+import falcon
+import json
+
+import spacy
+
+from MySerializers import TokenEncoder
+
+
+class TestEndpoint(object):
+
+    def on_get(self, req, resp):
+        body = {"value": "Ford"}
+        resp.body = json.dumps(body)
+
+
+class TestCompoundEndpoint(object):
+
+    def on_get(self, req, resp):
+        entry1 = {"value": "Ford A"}
+        entry2 = {"value": "Ford B"}
+
+        entry3 = {"value": "Ford C"}
+        body_list = [entry1, entry2, entry3]
+        outer_dict = {"list": body_list}
+        resp.body = json.dumps(outer_dict)
